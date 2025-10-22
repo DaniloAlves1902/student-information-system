fdce7e26bb7adac67acc1bb5487d8e6863355a4b
Danilo Alves <danilo.alvess1902@gmail.com>
2025-10-22 14:28:43 -0300

    Initial commit

---
+++ src/test/java/br/com/mandakaruconn/SIS/SisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mandakaruconn.SIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
